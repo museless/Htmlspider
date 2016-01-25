1ba4ede1f8e00d723649a09a5dbab5210365b8ae
museless <526659043@qq.com>
2016-01-25 10:19:07 +0800

    Adding some rules

--- sourcecode/python/bs.py
+++ sourcecode/python/bs.py
@@ -5,36 +5,13 @@ import chardet
 
 from datacatcher import DataCatcher
 
-
-#----------------------------------------------
-# News content obtainning way from html:
-#
-# 1. Pushing html content into BeautifulSoup
-#    and getting a bs entity.
-# 2. Getting <body> tag.
-# 3. Checking every body tag's children. If it
-#    has children too, keep digging.
-# 4. If a tag is childless, counting its chinese
-#    words number, recording it at a list like
-#    [tag handler, word number] and return it to
-#    its father.
-# 5. Finally, we make a list tree order by
-#    html structure.
-# 6. At this step, we will predefine a var, when
-#    node in list tree that its word number bigger
-#    than this var. We think its a valid node.
-# 7. We sorting those valid node. And chose a fit one.
-# 8. At last, printting out this tags scripped_string.
-#----------------------------------------------
-
-
 def open_html(path):
     with open(path) as file_desc:
         return  file_desc.read()
 
 if __name__ == "__main__":
     catcher = DataCatcher()
-    catcher.reading(open_html("cnfol.shtml"))
-    print catcher.title()
-    print catcher.data_source()
-    print catcher.news_content()
+    catcher.reading(open_html("qq.htm"), use_parser = "html.parser", decode = "gb2312")
+
+    print("%s\n%s" % catcher.title_and_data_source())
+    print(catcher.news_content())
--- sourcecode/python/example.py
+++ sourcecode/python/example.py
@@ -14,8 +14,10 @@ cursor = sql_handler.cursor()
 # get data catcher
 data_catcher = DataCatcher()
 
+table_name = "U20160122"
+
 while True:
-    cursor.execute("select * from U20160119 where State = 0 limit 1")
+    cursor.execute("select * from %s where State = 0 limit 1" % table_name)
     result = cursor.fetchone()
 
     if result == None:
@@ -31,32 +33,43 @@ while True:
 
     except:
         print("%s is wrong url" % url)
-        cursor.execute("update U20160119 set State = 3 where ID = %d" % result[0])
+        cursor.execute(
+        "update %s set State = 3 where ID = %d" % (table_name, result[0]))
         continue
 
     ret_code = url_data.getcode()
 
     if ret_code != 200:
         file_desc.write("cannot get: %s - code: %d\n\n" % (url, ret_code))
-        cursor.execute("update U20160119 set State = 2 where ID = %d" % result[0])
+        cursor.execute(
+        "update %s set State = 2 where ID = %d" % (table_name, result[0]))
+
         continue
 
     html_data = url_data.read()
 
     if len(html_data) < 100:
-        cursor.execute("update U20160119 set State = 4 where ID = %d" % result[0])
+        cursor.execute(
+        "update %s set State = 4 where ID = %d" % (table_name, result[0]))
+
         continue
 
-    data_catcher.reading(html_data)
-    file_desc.write("url: %s\n" % url) 
-    file_desc.write("title: %s\n" % data_catcher.title())
-    file_desc.write("data source: %s\n" % data_catcher.data_source())
+    if "qq.com" in url:
+        data_catcher.reading(
+        html_data, use_parser = "html.parser", decode = "gb2312")
+    else:
+        data_catcher.reading(html_data)
+
+    file_desc.write("Url: %s\n" % url)
+
+    file_desc.write(
+    "Title: %s\nSource: %s\n" % data_catcher.title_and_data_source())
 
     content = data_catcher.news_content()
-    content = (content == "") and data_catcher.title() or content
+    content = (content == "") and data_catcher.news_title or content
 
     file_desc.write("Content:\n%s\n\n\n" % content)
-    cursor.execute("update U20160119 set State = 1 where ID = %d" % result[0])
+    cursor.execute("update %s set State = 1 where ID = %d" % (table_name, result[0]))
 
 cursor.close()
 sql_handler.close()
