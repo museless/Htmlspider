25b1b51e75e59a7cfc18d95425325ac5a17a303e
Muse <526659043@qq.com>
2016-05-27 01:03:57 +0800

    New dictionary website

---
+++ text/dictionary/dictionarysite.py
@@ -0,0 +1,25 @@
+# -*- coding:utf8 -*-
+
+WebsiteMap = {
+    "马克思主义、列宁主义、毛泽东思想、邓小平理论": "http://www.dictall.com/zt/A/w1.htm",
+    "哲学、宗教": "http://www.dictall.com/zt/B/w1.htm",
+    "社会科学总论": "http://www.dictall.com/zt/C/w1.htm",
+    "政治、法律": "http://www.dictall.com/zt/D/w1.htm",
+    "军事": "http://www.dictall.com/zt/E/w1.htm",
+    "经济": "http://www.dictall.com/zt/F/w1.htm",
+    "文化、科学、教育、体育": "http://www.dictall.com/zt/G/w1.htm",
+    "语言、文字": "http://www.dictall.com/zt/H/w1.htm",
+    "文学": "http://www.dictall.com/zt/I/w1.htm",
+    "艺术": "http://www.dictall.com/zt/J/w1.htm",
+    "历史、地理": "http://www.dictall.com/zt/K/w1.htm",
+    "自然科学总论": "http://www.dictall.com/zt//w1.htm",
+    "数理科学和化学": "http://www.dictall.com/zt/O/w1.htm",
+    "天文学、地球科学": "http://www.dictall.com/zt/P/w1.htm",
+    "生物科学": "http://www.dictall.com/zt/Q/w1.htm",
+    "医药、卫生": "http://www.dictall.com/zt/R/w1.htm",
+    "农业科学": "http://www.dictall.com/zt/S/w1.htm",
+    "工业技术": "http://www.dictall.com/zt/T/w1.htm",
+    "交通运输": "http://www.dictall.com/zt/U/w1.htm",
+    "航空、航天": "http://www.dictall.com/zt/V/w1.htm",
+    "环境科学、安全科学": "http://www.dictall.com/zt/X/w1.htm",
+}
