551d65f9e0961c30d03936be9a81665681f52d1f
Muse <526659043@qq.com>
2016-07-05 00:36:30 +0800

    tester

--- sourcecode/c/src/tester/tester.c
+++ sourcecode/c/src/tester/tester.c
@@ -26,8 +26,7 @@
 -*----------------------------------------------*/
 
 #include "sp.h"
-
-#include "ping.h"
+#include <regex.h>
 
 
 /*----------------------------------------------
@@ -37,18 +36,6 @@
 /*----------------------------------------------
  *             Part Two: Local data
 -*----------------------------------------------*/
-
-static char         str_buff[64];
-static int          number;
-static int          test;
-static char         saver[33];
-
-static PerConfData  data[] = {
-    {"str", CONF_STR, str_buff, 64, "abc"},
-    {"number", CONF_NUM, &number, 4, "1000"},
-    {"data", CONF_NUM, &test, 1, "1"},
-    {"fucker", CONF_STR, saver, 32, "are you"},
-};
 
 
 /*----------------------------------------------
@@ -64,45 +51,9 @@ static PerConfData  data[] = {
 -*-----------------------------------------------*/
 
 
-#define nullptr NULL
-
-
-MATOS   atomic;
-
-
-void *rounte(void *para)
-{
-    while (true) {
-        while(!mato_dec_and_test(&atomic)) {
-            mato_inc(&atomic);
-        }
-
-        printf("%lu\n", pthread_self());
-    }
-
-    return  nullptr;
-}
-
-
 /*-----main-----*/
 int main(int argc, char **argv)
 {
-    /* mc_load_config("Tester", "../sourcecode/c/src/tester/test.conf");
-
-    mc_conf_read_list(data, sizeof(data) / sizeof(data[0]));
-
-    printf("%s - %d - %d\n", str_buff, number, test); */
-
-    pthread_t   thread;
-
-    mato_init(&atomic, 1); 
-
-    pthread_create(&thread, nullptr, rounte, nullptr);
-    pthread_create(&thread, nullptr, rounte, nullptr);
-    pthread_create(&thread, nullptr, rounte, nullptr);
-
-    while (true)
-        ;   /* nothing */
 
     return  -1;
 }
