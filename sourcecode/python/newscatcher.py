786a64965c3d85edd9f6c10cd2d021f34f68c369
Muse <526659043@qq.com>
2017-06-25 10:53:54 +0800

    modify: use thread pool replace thread alloc everytime, make something global

--- sourcecode/python/newscatcher.py
+++ sourcecode/python/newscatcher.py
@@ -23,26 +23,30 @@ from multiprocessing.pool import ThreadPool
 URL_ID_INDEX = 0
 URL_INDEX = 1
 
+Urlreceiver, Newsuploader = None, None
+Urltable, Newstable = None, None
+
 
 def catcher_initialize():
-    url_receiver = DataControl("Url");
-    news_uploader = DataControl("News", max_allow_packet = 48 * 1024)
+    global  Urlreceiver, Newsuploader
 
-    return  url_receiver, news_uploader
+    Urlreceiver = DataControl("Url");
+    Newsuploader = DataControl("News", max_allow_packet = 48 * 1024)
 
 def table_name_get():
+    global  Urltable, Newstable
+
     time_str = time.strftime("%Y%m%d")
+    Urltable, Newstable = "U" + time_str, "N" + time_str
 
-    return  "U" + time_str, "N" + time_str
-
-def url_data_get(url_receiver, url_tabname, url, url_id):
+def url_data_get(url, url_id):
     url_data = None
 
     try:
         url_data = urllib.urlopen(url)
 
-    except Exception, error_str:
-        url_receiver.update(1, url_tabname, 2, url_id)
+    except Exception:
+        Urlreceiver.update(1, Urltable, 2, url_id)
         return  None
 
     if url_data != None:
@@ -50,16 +54,16 @@ def url_data_get(url_receiver, url_tabname, url, url_id):
 
         if ret_code != 200:
             print("url: %s - return: %d" % (url, ret_code))
-            url_receiver.update(1, url_tabname, 2, url_id)
+            Urlreceiver.update(1, Urltable, 2, url_id)
             url_data = None
 
     return  url_data
 
-def html_data_get(url_receiver, url_tabname, url_data, url_id):
+def html_data_get(url_data, url_id):
     html = url_data.read()
 
     if len(html) < 256:
-        url_receiver.update(1, url_tabname, 3, url_id)
+        Urlreceiver.update(1, Urltable, 3, url_id)
         return  None
 
     return  html 
@@ -79,24 +83,24 @@ def html_extract(data_catcher, url, html, charset):
 
     data_catcher.reading(html, use_parser = "html.parser", decode = "gb2312")
 
-def upload_news(data_catcher, news_uploader, url_id, url, charset):
+def upload_news(data_catcher, url_id, url, charset):
     title, source = data_catcher.title_and_data_source(charset)
 
-    news_uploader.pre_insert(str(url_id), time.strftime("%H:%M"), \
-        source, news_uploader.escaping(title), url, \
-        news_uploader.escaping(data_catcher.news_content(charset)))
+    Newsuploader.pre_insert(str(url_id), time.strftime("%H:%M"), \
+        source, Newsuploader.escaping(title), url, \
+        Newsuploader.escaping(data_catcher.news_content(charset)))
 
-def handle_url_result(data_row, url_receiver, news_uploader, url_tabname):
+def handle_url_result(data_row):
     data_catcher = DataCatcher()
 
     url = data_row[URL_INDEX]
     url_id = data_row[URL_ID_INDEX]
 
-    url_receiver.update(1, url_tabname, 9, url_id)
-    url_data = url_data_get(url_receiver, url_tabname, url, url_id)
+    Urlreceiver.update(1, Urltable, 9, url_id)
+    url_data = url_data_get(url, url_id)
 
     if url_data != None:
-        html = html_data_get(url_receiver, url_tabname, url_data, url_id)
+        html = html_data_get(url_data, url_id)
 
         if html == None:
             return
@@ -104,40 +108,31 @@ def handle_url_result(data_row, url_receiver, news_uploader, url_tabname):
         charset = catch_html_charset(html) 
 
         if charset == False:
-            url_receiver.update(1, url_tabname, 4, url_id)
+            Urlreceiver.update(1, Urltable, 4, url_id)
             return
 
         html_extract(data_catcher, url, html, charset)
 
-        upload_news(data_catcher, news_uploader, url_id, url, charset)
-        url_receiver.update(1, url_tabname, 1, url_id)
+        upload_news(data_catcher, url_id, url, charset)
+        Urlreceiver.update(1, Urltable, 1, url_id)
 
-def catcher_work(url_receiver, news_uploader, 
-        url_tabname, news_tabname, url_limit = 8):
+def catcher_work(url_limit = 8):
+    Newsuploader.create(1, Newstable) 
+    Newsuploader.insert_ready(1, Newstable)
 
-    news_uploader.create(1, news_tabname) 
-    news_uploader.insert_ready(1, news_tabname)
+    pool = ThreadPool()
 
     while True:
-        url_receiver.select(1, url_tabname, url_limit)
-        results = url_receiver.cursor.fetchall()
+        Urlreceiver.select(1, Urltable, url_limit)
+        pool.map(handle_url_result, Urlreceiver.cursor.fetchall())
 
-        for data_row in results:
-            thread_entity = threading.Thread(
-                target = handle_url_result,
-                args = (data_row, url_receiver, news_uploader, url_tabname))
-
-            thread_entity.start()
-
-        time.sleep(2)
+        time.sleep(1)
 
 def main(argv = sys.argv):
-    url_receiver, news_uploader = catcher_initialize()
-    url_table, news_table = table_name_get()
-
-    catcher_work(url_receiver, news_uploader, url_table, news_table)
+    catcher_initialize()
+    table_name_get()
+    catcher_work()
 
 if __name__ == "__main__":
     main()
-
 
